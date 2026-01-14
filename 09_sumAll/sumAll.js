5b5d1a50c67c0341ca4413ae2e09e1dc588b56ab
Rifqi <mistermathindonesia@gmail.com>
2026-01-14 14:40:42 +0700

    finished exercise 08 and 09 removes from array and sum all array

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
 
+  if (min > max) {
+    // anggap seperti memindah 2 botol misal air dan teh
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let result = 0;
+  // mySolution
+  // if (Number.isInteger(min) && Number.isInteger(max)) {
+  //   for (let i = min; i <= max; i++) {
+  //     result += i;
+  //   }
+  //   return result;
+  // } else {
+  //   return "ERROR";
+  // }
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
+
+console.log(sumAll(3, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
