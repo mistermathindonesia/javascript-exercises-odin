194d89ed00b467ab4d043435bb6af47d20e487ee
Rifqi <mistermathindonesia@gmail.com>
2026-01-14 09:52:14 +0700

    finished exercise 07 reverse strings

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
 };
+reverseString("hello there");
+
+// console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
