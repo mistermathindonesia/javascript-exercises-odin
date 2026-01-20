e1fe3a6e146c27288c2b716084b322e5da3c3cf6
Rifqi <mistermathindonesia@gmail.com>
2026-01-20 14:20:08 +0700

    finish exercise 6 repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,15 +1,15 @@
 const repeatString = function (str, num) {
   let result = "";
   if (num >= 0) {
-    for (let i = 1; i <= num; i++) {
+    for (let i = 0; i < num; i++) {
       result += str;
     }
   } else {
-    // console.log("ERROR. Number must be greater than 0");
-    return "ERROR";
+    console.log("ERROR. Number must be greater than 0");
   }
   return result;
 };
-console.log(repeatString("iya", -3));
+
+console.log(repeatString("iya", 3));
 // Do not edit below this line
 module.exports = repeatString;
