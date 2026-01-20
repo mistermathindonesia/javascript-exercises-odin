fc510f1dac09c3e645a372c27204ad9e706db273
Rifqi <mistermathindonesia@gmail.com>
2026-01-20 14:20:35 +0700

    finish exercise 8 remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removeItem) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(removeItem);
+    if (!removeItem.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 1, 5, 4], 1, "a"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
