1b68ce6178d3060da59cf7dd8d72a02f402001f8
Rifqi <mistermathindonesia@gmail.com>
2026-01-24 10:34:47 +0700

    finished exercise 11 tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+  const result = ((5 * (deg - 32)) / 9).toFixed(1);
+  return parseFloat(result);
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100));
+
+const convertToFahrenheit = function (deg) {
+  const result = ((9 * deg) / 5 + 32).toFixed(1);
+  return parseFloat(result);
 };
+console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
